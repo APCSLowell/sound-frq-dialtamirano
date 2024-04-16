60449dac8c644addf68e8ea8bf6b56cc656eb8a5
dialtamirano <143028005+dialtamirano@users.noreply.github.com>
2024-04-15 22:14:28 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -3,31 +3,46 @@ public class Sound
 {
   /** the array of values in this sound; guaranteed not to be null */
   int[] samples;
-
-
-
+  
   /** Changes those values in this sound that have an amplitude greater than limit.
    *  Values greater than limit are changed to limit.
    *  Values less than -limit are changed to -limit.
    *  @param limit the amplitude limit
    *         Precondition: limit >= 0
-   *  @return the number of values in this sound that this method changed
-   */
+   *  @return the number of values in this sound that this method changed */
+  
   public int limitAmplitude(int limit)
-  {  
-    /* to be implemented in part (a) */
-    return 0;
+  { 
+  /* to be implemented in part(a) */
+  int changes = 0;
+   for(int i = 0; i < samples.length; i++){
+     if(samples[i] > limit){
+        samples[i] = limit;
+       changes++;
+     }
+    if(samples[i] < -limit){
+        samples[i] = -limit;
+       changes++;
+      }
+     }
+    return changes;
   }
-
-
 
   /** Removes all silence from the beginning of this sound.
    *  Silence is represented by a value of 0.
    *  Precondition: samples contains at least one nonzero value
-   *  Postcondition: the length of samples reflects the removal of starting silence
-   */
+   *  Postcondition: the length of samples reflects the removal of starting silence */
   public void trimSilenceFromBeginning()
   {
     /* to be implemented in part (b) */
+    int i = 0;
+    while(samples[i] == 0){
+       i++;
+    }
+    int[] newSamps = new int[samples.length-i];
+    for(int j = 0; j < newSamples.length; j++){
+      newSamples[j] = samples[j+n];
+    }
+    samples = newSamples;
   }
 }
